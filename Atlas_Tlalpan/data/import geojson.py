b601e6e9b691ea111bb66d46fc19f41925f1eeab
Xthelast <buendia_uriel@outlook.com>
2025-06-19 13:48:02 -0600

    Delete Atlas_Tlalpan directory

--- Atlas_Tlalpan/data/import geojson.py
+++
@@ -1,9 +0,0 @@
-import geojson
-
-def validar_geojson(data/Entidad_Tlalpan.geojson):
-        try:
-            with open(data/Entidad_Tlalpan.geojson, 'r') as f:
-                data = geojson.load(f)
-            print(f"El archivo {archivo_geojson} es válido.")
-        except ValueError as e:
-            print(f"El archivo {archivo_geojson} no es válido: {e}")
